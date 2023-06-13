0af88a17f9b16d03df8257c64ce49dec8df0ff54
Alok <alokk48200@gmail.com>
2023-06-13 19:59:59 +0530

    success

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-require('./DB/Connect')
+const connectDB=require('./DB/Connect')
+require('dotenv').config();
 const express=require('express');
 
- const app=express();
+const app=express();
 
 app.use(express.json());
 
    const task=require('./Routers/Task');
+const e = require('express');
    
    app.use('/api/task', task);
-
- app.listen(3000);
+   
+ const start=async()=>{
+  try {
+            await connectDB(process.env.uri);
+           app.listen(3000);
+           console.log("succesfully connect....");
+    
+  } catch (error) {
+      console.log(error);
+  }
+ }
+ start();
  
--- DB/Connect.js
+++ DB/Connect.js
@@ -1,7 +1,12 @@
 const mongoose=require('mongoose');
-const connectionString='mongodb+srv://alokk48200:Ak2020@nodejs.pi78j1a.mongodb.net/Nodejs?retryWrites=true&w=majority';
-mongoose.connect(connectionString).then((data)=>{
-    console.log("connection with databse............");
-}).catch((err)=>{
-    console.log(err);
-})
+const connectionString='';
+
+const connectDB=(url)=>{
+    return mongoose.connect(url).then((data)=>{
+        console.log("connection with databse............");
+    }).catch((e)=>console.log(e));
+}
+
+module.exports=connectDB;
+
+
